c4435cea0c3e2346c7cfd608e7b49d549d859621
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-10-02 15:29:52 +0300

    Add exceptions example

---
+++ examples/exceptions.py
@@ -0,0 +1,26 @@
+def parse_email(email_address):
+    username, domain = email_address.split("@")
+    if "." not in domain:
+        raise ValueError("invalid domain")
+    return username, domain
+
+
+addresses = [
+    "john.doe@gmail.com",
+    "test.com",
+    "hello@@world",
+    "jane@test.com",
+    100,
+    "maya@test",
+]
+
+
+for address in addresses:
+    try:
+        user, dom = parse_email(address)
+    except (ValueError, AttributeError) as ex:
+        print(f"Invalid address: {address} ({ex})")
+    else:
+        print(f"Welcome, {user}!")
+    finally:
+        print("Executes every time")
