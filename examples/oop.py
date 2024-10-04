f8a5ccfd2215710ee73f2b49d2dea872e2859f4c
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-10-04 10:11:45 +0300

    Add properties to Person

--- examples/oop.py
+++ examples/oop.py
@@ -3,11 +3,23 @@ from datetime import date
 
 class Person:
     count = 0  # class attribute
+    MIN_YEAR = 1900
 
     def __init__(self, name, date_of_birth=None):
         self.name = name  # instance attribute
         self.date_of_birth = date_of_birth
         self._increment_count()
+
+    @property
+    def date_of_birth(self):  # getter
+        return self._date_of_birth
+
+    @date_of_birth.setter
+    def date_of_birth(self, value):  # setter
+        if isinstance(value, date) and value.year >= self.MIN_YEAR:
+            self._date_of_birth = value
+        else:
+            raise ValueError(f"Invalid value for date_of_birth: {value}")
 
     @classmethod
     def _increment_count(cls):
@@ -21,31 +33,32 @@ class Person:
             age -= 1
         return age
 
-    def get_age(self):
-        return self.compute_age(self.date_of_birth)
+    @property
+    def age(self):  # computed attribute
+        return self.compute_age(self._date_of_birth)
 
     def greet(self, greeting=None):
         greeting = greeting or "hello"
         print(f"{greeting.capitalize()}, I am {self.name}!")
 
     def __str__(self):
-        return f"Person(name={self.name}, date_of_birth={self.date_of_birth})"
+        return f"Person(name={self.name}, date_of_birth={self._date_of_birth})"
 
     def __lt__(self, other):
-        return self.date_of_birth > other.date_of_birth
+        return self._date_of_birth > other.date_of_birth
 
     def __iadd__(self, other):
-        self.date_of_birth = date(self.date_of_birth.year - other,
-                                  self.date_of_birth.month,
-                                  self.date_of_birth.day)
+        self._date_of_birth = date(self._date_of_birth.year - other,
+                                  self._date_of_birth.month,
+                                  self._date_of_birth.day)
         return self
 
 
 if __name__ == "__main__":
     p1 = Person("Anna", date(1973, 5, 2))
     p2 = Person("Jane", date(2001, 6, 12))
-    print(p1.name, p2.name, p2.date_of_birth, p1.__dict__, p2.__dict__)
 
+    print(p1.name, p2.name, p2.date_of_birth, p1.__dict__, p2.__dict__)
     print(Person.count, p1.count, p1.count is Person.count)
 
     p1.greet('good morning')
@@ -62,4 +75,11 @@ if __name__ == "__main__":
     print(p1.date_of_birth)
 
     print(Person.compute_age(date(1918, 12, 1)))
-    print(p1.get_age())
+    print(f"{p1.name} is {p1.age} years old.")
+
+    print(p1.date_of_birth)  # get
+    try:
+        p1.date_of_birth = date(1890, 12, 1)  # set
+    except ValueError as ex:
+        print(ex)
+    # del p1.date_of_birth  # delete
